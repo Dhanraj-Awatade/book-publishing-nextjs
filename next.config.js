b99af669d9a2949ad6863b25a89c36f73b5a4df4
Dhanraj Awatade <dhanuawatade@gmail.com>
2024-07-26 02:26:44 +0530

    Development related changes

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ const nextConfig = {
     images:{
         remotePatterns:[
             {
-                hostname:"shabdashivar.in",
+                hostname:"saptarshee.in",
                 pathname:"**",
                 port:"3000",
                 protocol:"https"
@@ -40,7 +40,7 @@ const nextConfig = {
                 protocol:"http"
             },
             {
-                hostname:"192.168.205.211",
+                hostname:"192.168.1.211",
                 pathname:"**",
                 port:"3000",
                 protocol:"http"
