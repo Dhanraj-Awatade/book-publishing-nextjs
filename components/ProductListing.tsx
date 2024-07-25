dfbfe142b6d1b2b9416c8bcedaee3cd4c09b1bb7
Dhanraj Awatade <dhanuawatade@gmail.com>
2024-07-26 01:57:26 +0530

    Fixed Product Loading Placeholder

--- components/ProductListing.tsx
+++ components/ProductListing.tsx
@@ -63,12 +63,12 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
 export const ProductPlaceholder = () => {
     return (
         <div className='flex flex-col w-full'>
-            <div className='relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl'>
-                <Skeleton className='h-full w-full' />
+            <div className='relative aspect-square w-full overflow-hidden rounded-xl'>
+                <Skeleton className='h-full w-full bg-zinc-300' />
             </div>
-            <Skeleton className='mt-4 w-2/3 h-4 rounded-lg' />
-            <Skeleton className='mt-2 w-16 h-4 rounded-lg' />
-            <Skeleton className='mt-2 w-12 h-4 rounded-lg' />
+            <Skeleton className='mt-4 w-2/3 h-4 rounded-lg bg-zinc-300' />
+            <Skeleton className='mt-2 w-16 h-4 rounded-lg bg-zinc-300' />
+            <Skeleton className='mt-2 w-12 h-4 rounded-lg bg-zinc-300' />
         </div>
     )
 }
