66a811a8f2d33a7a5bb1d15a7134fb1571c0cf3b
Dhanraj Awatade <dhanuawatade@gmail.com>
2024-07-30 23:27:49 +0530

    Try Razorpay webhook

---
+++ app/api/razorWebhook/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import * as crypto from "crypto";
+import { getPayloadClient } from "@/lib/get-payload";
+import { User } from "../../../payload-types";
+import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils";
+
+export async function POST(req: Request, res: NextResponse) {
+  const data = await req.json();
+  const payload = await getPayloadClient();
+
+  const webhookSignature = req.headers.get("X-Razorpay-Signature") as string;
+  const webhookSecret = process.env.RAZORPAY_KEY as string;
+  const isValidated = validateWebhookSignature(
+    JSON.stringify(data),
+    webhookSignature,
+    webhookSecret
+  );
+
+  console.log(
+    "Webhook Body:",
+    JSON.stringify(data),
+    "isvalidated:",
+    isValidated
+  );
+  return NextResponse.json(data);
+}
