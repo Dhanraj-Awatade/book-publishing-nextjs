305379e79f496e082cb560acbb368da72d8a8d44
Dhanraj Awatade <dhanuawatade@gmail.com>
2024-09-07 23:50:39 +0530

    Cleanup code & handled error while fetching infinite products

--- trpc/index.ts
+++ trpc/index.ts
@@ -1,14 +1,9 @@
 import { z } from "zod";
 import { authRouter } from "./authRouter";
-import { privateProcedure, publicProcedure, router } from "./trpc";
-import {
-  PurchasedProductsQueryValidator,
-  QueryValidator,
-} from "../lib/validators/query-validator";
+import { publicProcedure, router } from "./trpc";
+import { QueryValidator } from "../lib/validators/query-validator";
 import { getPayloadClient } from "../lib/get-payload";
-import { Payload } from "payload";
 import { paymentRouter } from "./payment-router";
-import { Product } from "@/payload-types";
 import { productRouter } from "./productRouter";
 
 export const appRouter = router({
@@ -40,118 +35,83 @@ export const appRouter = router({
       const page = cursor || 1;
       // console.log("cursor:", cursor);
       // if (category && type) {
-      const {
-        docs: items,
-        hasNextPage,
-        nextPage,
-        hasPrevPage,
-        prevPage,
-      } = await payload.find({
-        collection: "products",
-        where: {
-          approvedForSale: {
-            equals: "approved",
+      try {
+        const {
+          docs: items,
+          hasNextPage,
+          nextPage,
+          hasPrevPage,
+          prevPage,
+        } = await payload.find({
+          collection: "products",
+          where: {
+            approvedForSale: {
+              equals: "approved",
+            },
+            ...parsedQueryOpts,
+            // category: {
+            //   equals: category,
+            // },
+            // type: {
+            //   equals: type,
+            // },
           },
-          ...parsedQueryOpts,
-          // category: {
-          //   equals: category,
-          // },
-          // type: {
-          //   equals: type,
-          // },
-        },
-        sort,
-        depth: 1,
-        limit,
-        page,
-      });
-      // console.log(
-      //   "hasNextPage:",
-      //   hasNextPage,
-      //   "nextPage:",
-      //   nextPage,
-      //   "hasPrevPage:",
-      //   hasPrevPage,
-      //   "prevPage:",
-      //   prevPage
-      // );
-      return {
-        items,
-        nextPage: hasNextPage ? nextPage : null,
-        prevPage: hasPrevPage ? prevPage : null,
-        hasNextPage,
-        hasPrevPage,
-      };
-      // } else if (type) {
-      //   const {
-      //     docs: items,
-      //     hasNextPage,
-      //     nextPage,
-      //   } = await payload.find({
-      //     collection: "products",
-      //     where: {
-      //       approvedForSale: {
-      //         equals: "approved",
-      //       },
-      //       type: {
-      //         equals: type,
-      //       },
-      //     },
-      //     sort,
-      //     depth: 1,
-      //     limit,
-      //     page,
-      //   });
-      //   return {
-      //     items,
-      //     nextPage: hasNextPage ? nextPage : null,
-      //   };
-      // } else if (category) {
-      //   const {
-      //     docs: items,
-      //     hasNextPage,
-      //     nextPage,
-      //   } = await payload.find({
-      //     collection: "products",
-      //     where: {
-      //       approvedForSale: {
-      //         equals: "approved",
-      //       },
-      //       category: {
-      //         equals: category,
-      //       },
-      //     },
-      //     sort,
-      //     depth: 1,
-      //     limit,
-      //     page,
-      //   });
-      //   return {
-      //     items,
-      //     nextPage: hasNextPage ? nextPage : null,
-      //   };
-      // } else {
-      //   const {
-      //     docs: items,
-      //     hasNextPage,
-      //     nextPage,
-      //   } = await payload.find({
-      //     collection: "products",
-      //     where: {
-      //       approvedForSale: {
-      //         equals: "approved",
-      //       },
-      //     },
-      //     sort,
-      //     depth: 1,
-      //     limit,
-      //     page,
-      //   });
-      //   return {
-      //     items,
-      //     nextPage: hasNextPage ? nextPage : null,
-      //   };
-      // }
+          sort,
+          depth: 1,
+          limit,
+          page,
+        });
+        // console.log(
+        //   "hasNextPage:",
+        //   hasNextPage,
+        //   "nextPage:",
+        //   nextPage,
+        //   "hasPrevPage:",
+        //   hasPrevPage,
+        //   "prevPage:",
+        //   prevPage,
+        //   "items:",
+        //   items
+        // );
+        return {
+          items,
+          nextPage: hasNextPage ? nextPage : null,
+          prevPage: hasPrevPage ? prevPage : null,
+          hasNextPage,
+          hasPrevPage,
+        };
+      } catch (error) {
+        console.log(
+          "Error fetching products, fetching next products; Error:",
+          error
+        );
+        const {
+          docs: items,
+          hasNextPage,
+          nextPage,
+          hasPrevPage,
+          prevPage,
+        } = await payload.find({
+          collection: "products",
+          where: {
+            approvedForSale: {
+              equals: "approved",
+            },
+            ...parsedQueryOpts,
+          },
+          sort,
+          depth: 1,
+          limit,
+          page: page + 1,
+        });
+        return {
+          items,
+          nextPage: hasNextPage ? nextPage : null,
+          prevPage: hasPrevPage ? prevPage : null,
+          hasNextPage,
+          hasPrevPage,
+        };
+      }
     }),
 });
 export type AppRouter = typeof appRouter;
