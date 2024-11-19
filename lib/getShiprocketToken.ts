637f3a4c6f8ebb30912d7a36c6df8b3634f2eaca
Dhanraj Awatade <dhanuawatade@gmail.com>
2024-11-20 04:47:22 +0530

    fix getShiprocketToken try#2

--- lib/getShiprocketToken.ts
+++ lib/getShiprocketToken.ts
@@ -19,12 +19,14 @@ export const getShiprocketToken = async () => {
             "https://apiv2.shiprocket.in/v1/external/auth/login",
             requestOptions
         );
-        const data = await req.json();
-        // .then((data) => (token = data.token))
-        // .then(() => createOrder())
-        // .catch((error) => console.log(error));
-        console.log("shiprocket token req success");
-        return data.token as string;
+        // const data = await req.json();
+
+        const data = await req
+            .json()
+            .then((data) => data.token)
+            .catch((error) => console.log(error));
+        console.log("shiprocket token req success", data);
+        return data as string;
     } catch (error) {
         console.log(error);
     }
